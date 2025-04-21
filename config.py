1585b292ccc78f4cb6d4caf3c542074233254488
isoko <isokoulin@mail.ru>
2025-04-21 18:30:38 +0300

    Скрытие токена и юзернэйма канала

--- config.py
+++ config.py
@@ -1,2 +1,4 @@
-BOT_TOKEN = '7517456212:AAGdfM5-nSsqAuWGhrQhpJFuvP-YvbG-pgc'
-CHANNEL_USERNAME = '@GuyWithSign'
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHANNEL_USERNAME = os.getenv("CHANNEL_USERNAME")
